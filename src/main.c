1d731290d8c003dec040a115f34e21a3753daf66
codelirious <131184319+codelirious@users.noreply.github.com>
2023-11-02 16:28:10 +0000

    SPECIAL: Work that will never be published but we will remember with a smile

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 10:47:12 by codespace         #+#    #+#             */
-/*   Updated: 2023/11/02 15:01:19 by codespace        ###   ########.fr       */
+/*   Updated: 2023/11/02 16:22:47 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,39 @@
 
 int	main(void)
 {
+	int		control;
+	char	*buff;
+
+	control = 0;
+	buff = ft_strdup("");
 	while (1)
 	{
-		ft_printf("MINICONCHA 🐚 => ");
-		g_ms.prompt = get_next_line(0);
-		if (strncmp(g_ms.prompt, "exit", 4))
+		if (control == 0)
 		{
-			printf("Prompt introducido: %s", g_ms.prompt);
-			add_history(g_ms.prompt);
-			free (g_ms.prompt);
+			ft_printf("CONCHITA 🐚 => ");
+			g_ms.prompt = get_next_line(0);
+			if (!ft_strncmp(g_ms.prompt, "exit", 4))
+				break ;
+			if (!control && ft_strchr(g_ms.prompt, '"') && ft_strchr(g_ms.prompt, '"') == ft_strrchr(g_ms.prompt, '"'))
+				control = 1;
+			else
+			{
+				printf("Prompt introducido: %s", g_ms.prompt);
+				add_history(g_ms.prompt);
+				free(g_ms.prompt);
+			}
 		}
-		else
-			break ;
+		else //CONTROL == 1
+		{
+			write(1, ">", 1);
+			buff = ft_strjoin(buff, g_ms.prompt);
+			g_ms.prompt = get_next_line(0);
+			if (ft_strchr(g_ms.prompt, '"'))
+			{
+				control = 0;
+				ft_printf("%s", buff + 1);
+			}
+		}
 	}
 	return (1);
 }
