d7b06e4177a3247ded34873de44d87751e87f54a
edejimen <edejimen@student.42urduliz.com>
2023-11-30 14:00:22 +0100

    feat: Appending redir (>>) implemented

--- src/redirecting.c
+++ src/redirecting.c
@@ -26,17 +26,16 @@ void	delete_lexer_token (t_parser_token *ptoken, t_lexer_token *ltoken)
 	}
 }
 
-void	check_redir_output(t_parser_token *ptoken) //redir = 3 '>'
+void	check_redir_output(t_parser_token *ptoken) //redir = 3 '>' && redir = 4 '>>'
 {
 	t_lexer_token	*ltoken;
 	t_lexer_token	*redir_token;
 	t_lexer_token	*redir_token_next;
 
-
 	ltoken = ptoken->lxr_list;
 	while (ltoken)
 	{
-		if (ltoken->tag_redir == 3)
+		if (ltoken->tag_redir == 3 || ltoken->tag_redir == 4)
 			{
 				redir_token = ltoken;
 				redir_token_next = ltoken->next;
@@ -49,17 +48,16 @@ void	check_redir_output(t_parser_token *ptoken) //redir = 3 '>'
 				{
 					if (ptoken->output_fd != 1)
 						close(ptoken->output_fd);
-					ptoken->output_fd = open (ltoken->next->arg, O_TRUNC | O_RDWR | O_CREAT, 0777);
+					if (ltoken->tag_redir == 3)
+						ptoken->output_fd = open (ltoken->next->arg, O_TRUNC | O_RDWR | O_CREAT, 0777);
+					if (ltoken->tag_redir == 4)
+						ptoken->output_fd = open (ltoken->next->arg, O_APPEND | O_RDWR | O_CREAT, 0777);
 					if(ptoken->output_fd == -1)
 						ft_putendl_fd("Permission denied", 2);
 				}
 				ltoken = ltoken->next->next;
 				delete_lexer_token(ptoken, redir_token);
-				/* free (redir_token->arg);
-				free (redir_token); */
 				delete_lexer_token(ptoken, redir_token_next);
-				/* free (redir_token_next->arg);
-				free (redir_token_next); */
 			}
 		else
 			ltoken = ltoken->next;
@@ -70,6 +68,5 @@ void	check_redirs(t_parser_token *ptoken)
 {
 	check_redir_output(ptoken);
 	//check_redir_input();
-	//check_redir_append();
 	//check_redir_here_doc();
 }
