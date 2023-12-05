edfb9664d68dbb9f525ff4da565cc8971041aa96
silvagomez <silva.camilo@gmail.com>
2023-12-05 10:33:24 +0100

    style: renaming files

---
+++ src/prompting.c
@@ -0,0 +1,41 @@
+
+#include "minishell.h"
+
+/*
+ * cd will update PWD
+ */
+void	set_pwd_prompt(t_ms *ms)
+{
+	size_t i;
+	//char	pwd[1024];
+
+	//update_env_wd(ms, "PWD", ft_getenv(ms, "OLDPWD"));
+	//free(ms->pwd);
+	//ms->pwd = ft_strdup(ft_getenv(ms, "PWD"));
+	if (ft_strncmp(ms->pwd, ms->home, ft_strlen(ms->home)) == 0)
+	{
+		//printf("PWD CONTIENE HOME \n%s\n%s\n", ms->pwd, ms->home);
+		i = ft_strlen(ms->home);
+		ms->pwd_ppt = ft_strjoin("~", ms->pwd + i);
+		//printf(HRED"HELLO1? %s\n"RST, ms->pwd_ppt);
+	}
+	else
+	{
+		free(ms->pwd_ppt);
+		ms->pwd_ppt = ft_strdup(ft_getenv(ms, "PWD"));
+		//printf(HRED"HELLO2? %s\n"RST, ms->pwd_ppt);
+	}
+}
+
+void	set_prompt(t_ms *ms)
+{
+	free(ms->rline);
+	free(ms->user);
+	ms->user = ft_strdup(ft_getenv(ms, "USER"));
+	set_pwd_prompt(ms);
+	ft_printf(HMAG"%s"HWHT" at "HMAG"CONCHITA"HWHT" in "CYN"%s 🐚 "\
+			RST, ms->user, ms->pwd_ppt);
+	ms->rline = readline("");
+	if (ms->rline && ms->rline[0] == 0)
+		printf("\n");
+}
---
+++ src/setting_defaults.c
@@ -0,0 +1,73 @@
+
+#include "minishell.h"
+
+/*
+ * This behind scene will launch a execve to get the uname
+ */
+void	set_os(t_ms *ms, char **envp)
+{
+	int			fd;
+	pid_t		pid;
+	const char	*cmd[] = {"/usr/bin/uname", 0, 0};
+	char		*line;
+
+	fd = open(OS_NAME, O_CREAT | O_TRUNC | O_RDWR, 0777);
+	if (fd < 0)
+		ft_putendl_fd("Error creating script file", 2);
+	pid=fork();
+	if (!pid)
+	{
+		dup2(fd, STDOUT_FILENO);
+		if(execve(cmd[0], (char **)cmd, envp) == -1)
+			printf("*+EXECVE FAILED+*\n");
+		exit(0);
+	}
+	else
+	{
+		close(fd);
+		waitpid(pid, 0, 0);
+		fd = open(OS_NAME, O_RDWR);
+		line = get_next_line(fd);
+		ms->os_name = ft_strdup(line);
+		*ft_strrchr(ms->os_name, '\n') = 0;
+		free(line);
+		close(fd);
+		unlink(OS_NAME);
+	}
+	printf(RED"Conchita is runnig at %s\n"RST, ms->os_name);
+}
+
+/*
+ * This function +1 to the env variable SHLVL.
+ */
+void	plus_one_shlvl(t_ms *ms)
+{
+	t_envlst	*tmp;
+	char		*content;
+	int			level;
+	
+	tmp = find_env(ms, "SHLVL");
+	if (!tmp)
+		ft_export(ms, "SHLVL=1");
+	tmp = find_env(ms, "SHLVL");
+	level = ft_atoi(tmp->content);
+	level++;
+	content = ft_itoa(level);
+	update_env_content(ms, "SHLVL", content);
+	free(content);
+}
+/*
+ * This function fills data to *ms varaibles and set a deault env variables when
+ * a shell is launching in this case *envlist.
+ */
+void	set_default_paths(t_ms *ms, char **envp)
+{
+	ms->user = ft_strdup(ft_getenv(ms, "USER"));
+	ms->home = ft_strdup(ft_getenv(ms, "HOME"));
+	ms->pwd = ft_strdup(ft_getenv(ms, "PWD"));
+	ms->pwd_ppt = ft_strdup(ft_getenv(ms, "PWD"));
+	update_env_content(ms, "_", "/usr/bin/env");
+	ft_unset(ms, "OLDPWD");
+	plus_one_shlvl(ms);
+	set_os(ms, envp);
+}
