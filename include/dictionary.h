9c783d248630ab52b6283d19756dc69854a19754
silvagomez <silva.camilo@gmail.com>
2023-12-05 10:35:24 +0100

    style: adding definitions

--- include/dictionary.h
+++ include/dictionary.h
@@ -2,9 +2,14 @@
 #ifndef DICTIONARY_H
 # define DICTIONARY_H
 
-# define WAIT 1 
+# define WAIT 1
+
+
+//ERRORS MSN
+
 
 // FILES BEHIND SCENE
-# define PID_BUFFER "temp"
+# define PID_BUFFER ".temp_pid"
+# define OS_NAME ".os_name"
 
 #endif
