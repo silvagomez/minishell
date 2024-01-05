8963418fef0c2da21c633e665a585e853a661878
silvagomez <silva.camilo@gmail.com>
2024-01-05 17:01:21 +0100

    style: cleaning comments

--- src/main.c
+++ src/main.c
@@ -13,13 +13,9 @@ int	main(int argc, char **argv, char **envp)
 		return (ft_putendl_fd("Env doesn't exist.", 2), -1);
 	//printf(HBLU"envp pointer %p\n"RST, envp);
 	fill_envp(&ms, envp);
-	ft_printf("ok-1\n");
 	set_default_paths(&ms, envp);
-	ft_printf("ok-2\n");
 	dollardollar(&ms, envp);
-	ft_printf("ok-3\n");
 	env_to_path(&ms, ms.envlst);
-	ft_printf("ok-4\n");
 	while (1)
 	{
 		//write(1, "\n\n\nENTRAMOS A BUCLE PRINCIPAL\n", 31);
