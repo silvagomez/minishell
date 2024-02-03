97ed3a93cfa24821929049e877e0e98882671913
silvagomez <silva.camilo@gmail.com>
2024-02-04 00:45:34 +0100

    refact: ft_unset)

--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -11,62 +11,41 @@ void	free_unset_node(t_envlst *node)
 	//return (free(node->content), free(node->name), free(node));
 }
 
+void	pointer_relocation(t_envlst **node, t_envlst **tmp)
+{
+	if ((*node)->prev && (*node)->next)
+	{
+		(*node)->next->prev = (*node)->prev;
+		(*node)->prev->next = (*node)->next;
+	}
+	else if ((*node)->prev && !(*node)->next)
+		(*node)->prev->next = NULL;
+	else if (!(*node)->prev && (*node)->next)
+	{
+		(*node)->next->prev = NULL;
+		(*tmp) = (*tmp)->next;
+	}
+}
+
 int	ft_unset(t_ms *ms, char *var_name)
 //void	ft_unset(t_ms *ms, t_lexer_token *ltoken)
 {
 	t_envlst	*tmp;
 	t_envlst	*node;
 
-	//if (!ltoken->arg)
 	if (!var_name)
 		return (0);
 	tmp = ms->envlst;
 	node = NULL;
-	//node = find_env(ms, ltoken->arg);
 	node = find_env(ms, var_name);
 	if (node)
 	{
-		//ft_printf(RED"moviendo nodos\n"RST);
-		if (node->prev && node->next)
-		{
-			//printf(HMAG"1prev %p ### %p %s %s ## next %p\n"RST, node->prev, node, node->name, node->content, node->next);
-			node->next->prev = node->prev;
-			node->prev->next = node->next;
-		}
-		else if (node->prev && !node->next)
-		{
-			//printf(MAG"2prev %p ### %p %s %s ## next %p\n"RST, node->prev, node, node->name, node->content, node->next);
-			node->prev->next = NULL;
-
-		}
-		else if (!node->prev && node->next)
-		{
-			//printf(HCYN"3prev %p ### %p %s %s ## next %p\n"RST, node->prev, node, node->name, node->content, node->next);
-			node->next->prev = NULL;
-			tmp = tmp->next;
-		}
+		pointer_relocation(&node, &tmp);
 		free_unset_node(node);
 		ms->envlst = tmp;
 	}
-	//ft_printf(RED"Size de envlst %i\n"RST, envlst_node_count(ms->envlst));
-	//ft_printf(RED"Size de tmp %i\n"RST, envlst_node_count(tmp));
 	if (!ft_strncmp(var_name, "PATH", ft_strlen(var_name) + 1))
 		error_handling(ERR_PATH, 127);
 	envlist_to_array(ms, UNSET);
-	//ft_printf(RED"uuuuuuu\n"RST);
-	/*
-	while (tmp)
-	{
-		if (!ft_strncmp(var_name, tmp->name, ft_strlen(var_name) + 1))
-		{
-			if (tmp->prev)
-				tmp->prev->next = tmp->next;
-			if (tmp->next)
-				tmp->next->prev = tmp->prev;
-			return (free(tmp->content), free(tmp->name), free(tmp));
-		}
-		tmp = tmp->next;
-	}
-	*/
 	return (0);
 }
