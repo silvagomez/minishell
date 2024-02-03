975c89887b1d2dacd7f417333f09c1ded08f87e4
silvagomez <silva.camilo@gmail.com>
2024-02-04 00:47:56 +0100

    clean: export_utils1

--- src/builtins/export_utils1.c
+++ src/builtins/export_utils1.c
@@ -40,14 +40,12 @@ void	memory_address_relocation(t_envlst **tmp2, t_envlst **tmp0)
 t_envlst	*get_sorted_envlst(t_envlst *envlst)
 {
 	t_envlst	*sorted_envlst;
-	//t_envlst	*srtd_envlst;
 	t_envlst	*tmp0;
 	t_envlst	*tmp1;
 	t_envlst	*tmp2;
 
 	tmp0 = dup_envlst(envlst);
 	sorted_envlst = NULL;
-	//srtd_envlst = NULL;
 	tmp1 = tmp0;
 	while (tmp1->next)
 	{
@@ -58,16 +56,12 @@ t_envlst	*get_sorted_envlst(t_envlst *envlst)
 				tmp2 = tmp1;
 			tmp1 = tmp1->next;
 		}
-		//dup_envlst_add(&srtd_envlst, dup_envlst_new(&srtd_envlst, tmp2));
 		memory_address_relocation(&tmp2, &tmp0);
 		dup_envlst_add(&sorted_envlst, tmp2);
-		//free_env_node(&tmp2);
 		tmp1 = tmp0;
 		if (tmp1->next == NULL)
-			//dup_envlst_add(&srtd_envlst, dup_envlst_new(&srtd_envlst, tmp1));
 			dup_envlst_add(&sorted_envlst, tmp1);
 	}
-	//return (free_env_node(&tmp1), srtd_envlst);
 	return (sorted_envlst);
 }
 
