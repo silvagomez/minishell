bad4cbc83c0d873ffc872fac5a29f9fed53e5f0d
silvagomez <silva.camilo@gmail.com>
2024-02-06 13:35:37 +0100

    refact: we need to refact this -> check_redir_input(t_parser_token *ptoken)

--- src/redirecting0.c
+++ src/redirecting0.c
@@ -26,6 +26,28 @@ void	delete_lexer_token (t_parser_token *ptoken, t_lexer_token *ltoken)
 	}
 }
 
+//redir output = 3 '>' && redir = 4 '>>'
+int	redir_output_fd(t_parser_token *ptoken, t_lexer_token *ltoken)
+{
+	if (!ltoken->next)
+		return (error_handling(ERR_RDIR, 258), 1);
+	else
+	{
+		ptoken->is_output = 1;
+		if (ptoken->output_fd != 1)
+			close(ptoken->output_fd);
+		if (ltoken->tag_redir == 3)
+			ptoken->output_fd = open (ltoken->next->arg, \
+					O_TRUNC | O_RDWR | O_CREAT, 0777);
+		if (ltoken->tag_redir == 4)
+			ptoken->output_fd = open (ltoken->next->arg, \
+					O_APPEND | O_RDWR | O_CREAT, 0777);
+		if(ptoken->output_fd == -1)
+			return (error_handling(ERR_DFLT, EXIT_FAILURE), 1);
+	}
+	return (0);
+}
+
 //redir = 3 '>' && redir = 4 '>>'
 int	check_redir_output(t_parser_token *ptoken)
 {
@@ -40,20 +62,8 @@ int	check_redir_output(t_parser_token *ptoken)
 			{
 				redir_token = ltoken;
 				redir_token_next = ltoken->next;
-				if (!ltoken->next)
-					return (error_handling(ERR_RDIR, 258), 1);
-				else
-				{
-					ptoken->is_output = 1;
-					if (ptoken->output_fd != 1)
-						close(ptoken->output_fd);
-					if (ltoken->tag_redir == 3)
-						ptoken->output_fd = open (ltoken->next->arg, O_TRUNC | O_RDWR | O_CREAT, 0777);
-					if (ltoken->tag_redir == 4)
-						ptoken->output_fd = open (ltoken->next->arg, O_APPEND | O_RDWR | O_CREAT, 0777);
-					if(ptoken->output_fd == -1)
-						return (error_handling(ERR_DFLT, EXIT_FAILURE), 1);
-				}
+				if (redir_output_fd(ptoken, ltoken))
+						return (EXIT_FAILURE);
 				ltoken = ltoken->next->next;
 				delete_lexer_token(ptoken, redir_token);
 				delete_lexer_token(ptoken, redir_token_next);
@@ -64,6 +74,32 @@ int	check_redir_output(t_parser_token *ptoken)
 	return (0);
 }
 
+int	redir_input_fd(t_parser_token *ptoken, t_lexer_token *ltoken, \
+		t_lexer_token **redir_token, t_lexer_token **redir_token_next)
+{
+			if (!ltoken->next)
+				return (error_handling(ERR_RDIR, 258), 1);
+			else
+			{
+				ptoken->is_input = 1;
+				(*redir_token) = ltoken;
+				(*redir_token_next) = ltoken->next;
+				if (ptoken->input_fd != 0)
+					close(ptoken->input_fd);
+				if (ltoken->tag_redir == 1)
+					ptoken->input_fd = open (ltoken->next->arg, O_RDONLY);
+				if (ltoken->tag_redir == 2)
+				{
+					ptoken->is_here_doc = 1;
+					hdlst_add(&(ptoken->hd_list), hdlst_new(ltoken->next->arg));
+				}
+				if(ptoken->input_fd == -1)
+					return (error_handling(ERR_DFLT, EXIT_FAILURE), 1);
+			}
+			return (0);
+}
+
+//redir = 1 '<' && redir = 2 '<<'
 int	check_redir_input(t_parser_token *ptoken)
 {
 	t_lexer_token	*ltoken;
@@ -71,33 +107,37 @@ int	check_redir_input(t_parser_token *ptoken)
 	t_lexer_token	*redir_token_next;
 
 	ltoken = ptoken->lxr_list;
+	redir_token = NULL;
+	redir_token_next = NULL;
 	while (ltoken)
 	{
 		if (ltoken->tag_redir == 1 || ltoken->tag_redir == 2)
+		{
+			if (!ltoken->next)
+				return (error_handling(ERR_RDIR, 258), 1);
+			else
 			{
-				if (!ltoken->next)
-					return (error_handling(ERR_RDIR, 258), 1);
-				else
+				ptoken->is_input = 1;
+				redir_token = ltoken;
+				redir_token_next = ltoken->next;
+				if (ptoken->input_fd != 0)
+					close(ptoken->input_fd);
+				if (ltoken->tag_redir == 1)
+					ptoken->input_fd = open (ltoken->next->arg, O_RDONLY);
+				if (ltoken->tag_redir == 2)
 				{
-					ptoken->is_input = 1;
-					redir_token = ltoken;
-					redir_token_next = ltoken->next;
-					if (ptoken->input_fd != 0)
-						close(ptoken->input_fd);
-					if (ltoken->tag_redir == 1)
-						ptoken->input_fd = open (ltoken->next->arg, O_RDONLY);
-					if (ltoken->tag_redir == 2)
-					{
-						ptoken->is_here_doc = 1;
-						hdlst_add(&(ptoken->hd_list), hdlst_new(ltoken->next->arg));
-					}
-					if(ptoken->input_fd == -1)
-						return (error_handling(ERR_DFLT, EXIT_FAILURE), 1);
+					ptoken->is_here_doc = 1;
+					hdlst_add(&(ptoken->hd_list), hdlst_new(ltoken->next->arg));
 				}
-				ltoken = ltoken->next->next;
-				delete_lexer_token(ptoken, redir_token_next);
-				delete_lexer_token(ptoken, redir_token);
+				if(ptoken->input_fd == -1)
+					return (error_handling(ERR_DFLT, EXIT_FAILURE), 1);
 			}
+			//if (redir_input_fd(ptoken, ltoken, &redir_token, &redir_token))
+			//	return (EXIT_FAILURE);
+			ltoken = ltoken->next->next;
+			delete_lexer_token(ptoken, redir_token_next);
+			delete_lexer_token(ptoken, redir_token);
+		}
 		else
 			ltoken = ltoken->next;
 	}
